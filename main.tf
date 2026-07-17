59e086c43d679429d52069bf3a12299e9917dbaa
Finn <finnh990@gmail.com>
2026-07-17 18:57:42 +1000

    public subnet fix

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ module "blog_vpc" {
   name = "dev"
   cidr = "10.0.0.0/16"
 
-  azs             = ["ap-southeast-4a", "ap-southeast-4b", "ap-southeast-4c"]
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
 
@@ -57,10 +57,10 @@ module "blog_alb" {
   source = "terraform-aws-modules/alb/aws"
 
   name    = "blog-alb"
-  vpc_id  = "module.blog_vpc.vpc_id"
+  vpc_id  = module.blog_vpc.vpc_id
   subnets = module.blog_vpc.public_subnets
 
-security_groups = [module.blog_sg.security_group_id]
+  security_groups = [module.blog_sg.security_group_id]
 
   listeners = {
     blog-http = {
