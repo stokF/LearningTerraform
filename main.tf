4ba8ca5d376b7f4c73a7eb2466434db6eb646088
Finn <finnh990@gmail.com>
2026-07-17 17:12:38 +1000

    Complete Overhall - Branch03_04

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ data "aws_ami" "app_ami" {
 
   filter {
     name   = "name"
-    values = ["al2023-ami-2023*-x86_64"]
+    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
   }
 
   filter {
@@ -11,57 +11,56 @@ data "aws_ami" "app_ami" {
     values = ["hvm"]
   }
 
-  owners = ["amazon"] # Amazon Linux 2023
+  owners = ["979382823631"] # Bitnami
 }
 
 data "aws_vpc" "default" {
   default = true
 }
 
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
+resource "aws_instance" "blog" {
+  ami                    = data.aws_ami.app_ami.id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [aws_security_group.blog.id]
 
-  vpc_security_group_ids = [aws_security_group.web.id]
-
-  tags   = {
-    Name = "LearningTerraform"
+  tags = {
+    Name = "Learning Terraform"
   }
 }
 
-resource "aws_security_group" "web" {
-  name        = "web"
-  description = "Allow http and https in. Allow everything out."
-
-  vpc_id      = data.aws_vpc.default.id
+resource "aws_security_group" "blog" {
+  name = "blog"
+  tags = {
+    Terraform = "true"
+  }
+  vpc_id = data.aws_vpc.default.id
 }
 
-resource "aws_security_group_rule" "web_http_in" {
+resource "aws_security_group_rule" "blog_http_in" {
   type        = "ingress"
   from_port   = 80
-  to_potr     = 80
+  to_port     = 80
   protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0//0"]
-
-  aws_security_group_id = aws_security_group.web.id
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.blog.id
 }
 
-resource "aws_security_group_rule" "web_https_in" {
+
+resource "aws_security_group_rule" "blog_https_in" {
   type        = "ingress"
   from_port   = 443
-  to_potr     = 443
+  to_port     = 443
   protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0//0"]
-
-  aws_security_group_id = aws_security_group.web.id
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.blog.id
 }
 
-resource "aws_security_group_rule" "web_everything_out" {
+
+resource "aws_security_group_rule" "blog_everything_out" {
   type        = "egress"
   from_port   = 0
-  to_potr     = 0
+  to_port     = 0
   protocol    = "-1"
-  cidr_blocks = ["0.0.0.0//0"]
-
-  aws_security_group_id = aws_security_group.web.id
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.blog.id
 }
